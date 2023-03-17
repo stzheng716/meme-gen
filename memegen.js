933ed098c288fb3884b0e367297a0bed99531bff
Steven <stzheng716@gmail.com>
2023-03-17 16:24:18 -0700

    add meme text and removal function

--- memegen.js
+++ memegen.js
@@ -1,16 +1,36 @@
 
 const imageURL = document.querySelector('#imageURL')
+const topInput = document.querySelector('#topText')
 const bottomInput = document.querySelector('#bottomText')
 const memeSpace = document.querySelector('#memeSpace')
 
 
 const form = document.querySelector('#memeform')
 form.addEventListener('submit', function(event) {
-    const newImg = document.createElement('img')
-    newImg.setAttribute('src', imageURL.value)
-    newImg.classList.add('thumbnail')
-    memeSpace.append(newImg)
-    imageURL.value = ''
+    const memeDiv = document.createElement('div');
+    memeDiv.classList.add('thumbnail','memeDiv');
+    memeDiv.addEventListener('click', function(event){
+        memeDiv.remove()
+    })
+    
+    const newImg = document.createElement('img');
+    newImg.setAttribute('src', imageURL.value);
+    newImg.classList.add('thumbnail',);
+    memeDiv.append(newImg);
+
+    const topText = document.createElement('span');
+    topText.innerText = topInput.value;
+    const bottomText = document.createElement('span');
+    bottomText.innerText = bottomInput.value;
+    topText.classList.add('absolutePositionTop', 'memeText');
+    bottomText.classList.add('absolutePositionBottom', 'memeText');
+    memeDiv.append(topText, bottomText);
+    memeSpace.append(memeDiv);
+    
+    imageURL.value = '';
+    topInput.value = '';
+    bottomInput.value = '';
+
     event.preventDefault()
 })
 
