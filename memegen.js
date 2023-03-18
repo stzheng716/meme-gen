660d2a9df6a133cd6f99fd6a97c7f3f5b4ddaff0
Steven <stzheng716@gmail.com>
2023-03-17 20:19:13 -0700

    add bootstrap styles and rbg letters

--- memegen.js
+++ memegen.js
@@ -38,7 +38,20 @@ form.addEventListener('submit', function(event) {
     event.preventDefault()
 })
 
+const letters = document.querySelectorAll('.letter');
 
+function randomRGB(){
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r},${g},${b})`
+}
+
+setInterval(function(){
+    for(let letter of letters){
+        letter.style.color = randomRGB()
+    }
+}, 1000)
 
 
 
